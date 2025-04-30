c14df3b803d5752194d3277a95a2074c0ec8a4d1
vitax <vitaleantonio.1at@gmail.com>
2025-04-30 23:22:13 +0200

    per rendere gli algoritmi più simili, ho modificato il tipo di gantt da dizionario a lista (di tuple)

--- fcfs.py
+++ fcfs.py
@@ -1,3 +1,11 @@
+def create_table(procs_num, procs_order):
+    table = {} #table:   {Processo: CPU_burst}
+    for i in range(procs_num):
+        n = procs_order.split(",")[i]
+        cpu_burst = int(input(f"Durata picco del processo P{n} (in secondi)"))
+        table[f"P{n}"] = cpu_burst
+
+        return table
 def wait_time(gantt, procs_order):
     total_wait_time = 0
     wait_times = []
@@ -16,18 +24,11 @@ def print_gantt(gantt):
         proc_time = value[1] - value[0]
         print(f"{key}({proc_time}) -> ", end="")
 
-
 procs_num = int(input("Inserire il numero dei processi in coda: "))
-
 #indico in che ordine arrivano
 procs_order = input("Indica l'ordine di arrivo dei processi in coda. Ad esempio, "
           "scrivendo 2,3,1 stai indicando che i processi arrivano nell'ordine P2, P3, P1 ")
-
-table = {} #table:   {Processo: CPU_burst}
-for i in range(procs_num):
-    n = procs_order.split(",")[i]
-    cpu_burst = int(input(f"Durata picco del processo P{n} (in secondi)"))
-    table[f"P{n}"] = cpu_burst
+table = create_table(procs_num, procs_order)
 
 #calcolo per ogni processo il momento di entrata e il momento di uscita dalla coda (in secondi) e li inserisco nella tupla
 gantt = [] #gantt: [(Processo, (momento di entrata, momento di uscita))
