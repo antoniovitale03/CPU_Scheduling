1ed0de977d1f2687472ba29bb48ab70eca89c3f3
vitax <vitaleantonio.1at@gmail.com>
2025-05-02 12:22:51 +0200

    finished sfj_preemptive

--- sfj_preemptive.py
+++ sfj_preemptive.py
@@ -117,8 +117,8 @@ procs_num = int(input("Inserire il numero dei processi in coda: "))
 procs_order = input("Indica l'ordine di arrivo dei processi in coda. Ad esempio, "
                         "scrivendo 2,3,1 stai indicando che i processi arrivano nell'ordine P2, P3, P1 ")
 table = create_table(procs_num, procs_order)
-gantt, table, t = sjf_preemptive(table)
-gantt = sfj_non_preemptive(gantt, table, t)
-#print(table)
+gantt, table, t = sjf_preemptive(table)  #applico la prelazione ai processi
+gantt = sfj_non_preemptive(gantt, table, t)  #proseguo con lo stesso metodo del sfj preemptive, non applico più prelazione
+
 print_gantt(gantt)
-#wait_time(gantt, procs_order)
+wait_time(gantt, procs_order)
